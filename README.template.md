453fa593584f91bb3142b915d5b3756a3644d69d
Will Weaver <monkey@buildingbananas.com>
2020-09-01 03:35:33 +0000

    Switch to python for notion-py, get a quick implementation done

---
+++ README.template.md
@@ -0,0 +1,9 @@
+### Hi there 👋
+
+I'm Will. I like to play games and desparately try to automate myself away from ever having to do the same thing multiple times. Thankfully there is inevitably more things to automate, and more games to play so I'm very rarely bored.
+
+### Recently Added Bookmarks 
+
+{{#blogmarks}}
+* [{{title}}]({{url}})
+{{/blogmarks}}
---
+++ readme_builder.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+import chevron
+NOTION_V2_TOKEN = os.getenv("NOTION_V2_TOKEN")
+BLOGMARKS_URL = "https://www.notion.so/380ce82124f74f959e25a03a31f696b4?v=5297dd0f955742128043cd769204ecfc"
+
+from notion.client import NotionClient
+
+client = NotionClient(token_v2=NOTION_V2_TOKEN)
+
+
+# Get all blogmarks and sort by timestamp, most recent first.
+cv = client.get_collection_view(BLOGMARKS_URL)
+
+sort_params = [{
+    "direction": "descending",
+    "property": "created"
+}]
+
+result = cv.build_query(sort=sort_params).execute()
+
+# Get public results because I'm struggling to get this filter working with the API
+public = list(filter(lambda row: row.public == True, result))
+# Convert to display dictionaries
+output = list(map(lambda row: { "title": row.title, "url": row.url }, public))
+
+
+with open('README.template.md', 'r') as f:
+    file_content = chevron.render(f, { "blogmarks": output })
+
+with open('README.md', 'w') as f:
+    f.write(file_content)
+    f.close
+
+# Print the README.md
+with open('README.md', 'r') as f:
+    print(f.read())
