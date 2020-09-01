51f0e45c8dcda6b030c6583d274b997b53e0f987
Will Weaver <monkey@buildingbananas.com>
2020-09-01 17:40:39 +0000

    extracting methods for readability and testability

--- build_readme.py
+++ build_readme.py
@@ -3,37 +3,45 @@ load_dotenv()
 
 import os
 import chevron
+from notion.client import NotionClient
+
 NOTION_V2_TOKEN = os.getenv("NOTION_V2_TOKEN")
 BLOGMARKS_URL = "https://www.notion.so/380ce82124f74f959e25a03a31f696b4?v=5297dd0f955742128043cd769204ecfc"
 
-from notion.client import NotionClient
+def main(notion_client):
+    result = getAllBlogmarks(notion_client)
+    public = get_public(result)
+    readme_content = prepare_readme(public)
+    write_and_print('README.md', readme_content)
 
-client = NotionClient(token_v2=NOTION_V2_TOKEN)
+def getAllBlogmarks(client):
+    cv = client.get_collection_view(BLOGMARKS_URL)
 
+    sort_params = [{
+        "direction": "descending",
+        "property": "created"
+    }]
+    
+    return cv.build_query(sort=sort_params).execute()
 
-# Get all blogmarks and sort by timestamp, most recent first.
-cv = client.get_collection_view(BLOGMARKS_URL)
+def get_public(result):
+    return list(filter(lambda row: row.public == True, result))
 
-sort_params = [{
-    "direction": "descending",
-    "property": "created"
-}]
+def prepare_readme(public):
+    most_recent = public[:5]
+    blogmarks = { "blogmarks": list(map(lambda row: { "title": row.title, "url": row.url }, most_recent)) }
+    with open('README.template.md', 'r') as f:
+        file_content = chevron.render(f, blogmarks)
+    return file_content
 
-result = cv.build_query(sort=sort_params).execute()
+def write_and_print(file_name, content):
+    with open(file_name, 'w') as f:
+        f.write(content)
+        f.close
 
-# Get public results because I'm struggling to get this filter working with the API
-public = list(filter(lambda row: row.public == True, result))
-# Convert to display dictionaries
-top_five = list(map(lambda row: { "title": row.title, "url": row.url }, public[:5]))
+    with open(file_name, 'r') as f:
+        print(f.read())
+        f.close
 
-
-with open('README.template.md', 'r') as f:
-    file_content = chevron.render(f, { "blogmarks": top_five })
-
-with open('README.md', 'w') as f:
-    f.write(file_content)
-    f.close
-
-# Print the README.md
-with open('README.md', 'r') as f:
-    print(f.read())
+notion_client = NotionClient(token_v2=NOTION_V2_TOKEN)
+main(notion_client)
